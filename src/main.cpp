ccd27685b773b7ffdb0ad3207d10e625a8a694c7
Oleg Danko <olegdanko@outlook.com>
2023-03-24 00:47:59 +0200

    Simulating mouse clicks events using Xtst

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,93 @@
 #include <array>
 #include <future>
 #include <sstream>
+#include <optional>
+#include <iomanip>
+#include <X11/Xlib.h>
+#include <X11/extensions/XTest.h>
 
 #include <websocket/websocketpp/server/GenWSPPServer.hpp>
 
-void exec(const std::string& cmd) {
-    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"), pclose);
-}
+class InputClientX11 {
+    Display *display;
+    Window root_window;
+public:
+    InputClientX11()
+        : display(XOpenDisplay(NULL))
+        , root_window(DefaultRootWindow(display)) {}
+    ~InputClientX11() {
+        XCloseDisplay(display);
+    }
 
-using ss = std::stringstream;
+    std::optional<XEvent> query_pointer(Window& window) {
+        XEvent event;
+        if(0 == XQueryPointer(display,
+                              window,
+                              &event.xbutton.root,
+                              &event.xbutton.subwindow,
+                              &event.xbutton.x_root,
+                              &event.xbutton.y_root,
+                              &event.xbutton.x,
+                              &event.xbutton.y,
+                              &event.xbutton.state)) {
+            return {};
+        }
+        return {event};
+    }
+
+    std::optional<std::tuple<int, int>> get_pointer_position() {
+        if(auto event_opt = query_pointer(root_window); event_opt) {
+            auto event_btn = event_opt.value().xbutton;
+            return {std::make_tuple(event_btn.x_root, event_btn.y_root)};
+        }
+
+        std::cout << "failed onto retreive current pointer position" << std::endl;
+        return {};
+
+    }
+
+    void set_pointer_position(Display *display, Window& root, int x, int y) {
+        XWarpPointer(display, None, root, 0, 0, 0, 0, x, y);
+        XFlush(display);
+    }
+
+    void move_cursor(int x, int y) {
+        XTestFakeRelativeMotionEvent(display, x, y, 0);
+        XFlush(display);
+        return;
+        if(auto p_opt = get_pointer_position(); p_opt) {
+            auto [pos_x, pos_y] = p_opt.value();
+            set_pointer_position(display, root_window, pos_x + x, pos_y + y);
+        }
+    }
+
+    std::optional<XEvent> query_pointer_leaf_window() {
+        XEvent event;
+        event.xbutton.subwindow = root_window;
+        while (event.xbutton.subwindow) {
+            std::cout << "checking " << event.xbutton.subwindow << std::endl;
+            auto event_opt = query_pointer(event.xbutton.subwindow);
+            if(!event_opt) {
+                std::cout << "failed to query leaf event" << std::endl;
+                return {};
+            }
+            event = event_opt.value();
+        }
+        std::cout << "Window containing pointer is " << event.xbutton.window << std::endl;
+        return {event};
+    }
+
+    void button_press(int button) {
+        XTestFakeButtonEvent(display, button, True, CurrentTime);
+        XFlush(display);
+    }
+    void button_release(int button) {
+        XTestFakeButtonEvent(display, button, False, CurrentTime);
+        XFlush(display);
+    }
+};
+
+InputClientX11 input_clint;
 
 double x{0}, y{0};
 
@@ -19,30 +98,37 @@ void mouse_move_end_apply() {
     x = y = 0;
 }
 
+using ss = std::stringstream;
+
 void mouse_move_apply(ss& s) {
     double x_in, y_in;
     s >> x_in >> y_in;
 
+    double exp_base = 1.00005;
+    x_in *= std::pow(exp_base, std::abs(x_in));
+    y_in *= std::pow(exp_base, std::abs(y_in));
+
     x += x_in;
     y += y_in;
 
-    double x_out = std::round(x / 200);
-    double y_out = std::round(y / 200);
+    int div = 200;
 
-    x -= x_out * 200;
-    y -= y_out * 200;
+    double x_out = std::round(x / div);
+    double y_out = std::round(y / div);
+
+    x -= x_out * div;
+    y -= y_out * div;
 
     int x_out_int = x_out;
     int y_out_int = y_out;
-    exec((ss() <<
-          "xdotool mousemove_relative -- "
-          << x_out_int << " "
-          << y_out_int).str());
+
+    input_clint.move_cursor(x_out_int, y_out_int);
 }
+
 
 std::unordered_map<std::string, int> btn_map{
     {"lmb", 1},
-    {"lmb", 2},
+    {"mmb", 2},
     {"rmb", 3}
 };
 
@@ -55,9 +141,7 @@ void press_apply(ss& s) {
         return;
     }
 
-    auto cmd = (ss() << "xdotool mousedown " << btn_map[btn]).str();
-    std::cout << cmd << std::endl;
-    exec(cmd);
+    input_clint.button_press(btn_map[btn]);
 }
 void release_apply(ss& s) {
     std::string btn;
@@ -68,12 +152,16 @@ void release_apply(ss& s) {
         return;
     }
 
-    auto cmd = (ss() << "xdotool mouseup " << btn_map[btn]).str();
-    std::cout << cmd << std::endl;
-    exec(cmd);
+    input_clint.button_release(btn_map[btn]);
 }
 
 void interpret_message(const std::string& msg) {
+    static const auto start = std::chrono::steady_clock::now();
+
+    auto since_start = std::chrono::steady_clock::now() - start;
+
+    std::cout << since_start.count()
+              << std::endl;
     ss s(msg);
     std::string cmd;
     s >> cmd;
